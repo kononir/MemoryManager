f803cc7fe2f2fb644a6ed443f737ec549e486db1
Vlad <novlad26@gmail.com>
2018-09-22 13:22:26 +0300

    Wrote function _find_free_space

--- MemoryManager/MemoryManager.cpp
+++ MemoryManager/MemoryManager.cpp
@@ -6,54 +6,42 @@
 
 typedef char* VA;						// Тип описывающий адрес блока (в ОП)
 
+typedef struct physicalBlock
+{
+	long offset;
+	long size;
+	struct physicalBlock* next;
+} physicalBlock;
+
 typedef struct physicalMemory			//ОП
 {
-	long size;							//размер памяти
 	VA space;							//собственно, указатель на память
+	long size;
+	physicalBlock* head;
 } physicalMemory;
 
-typedef struct virtualSegmentBlock		//блок виртуального сегмента
-{
-	long offset;						//смещение блока
-	struct virtualSegmentBlock* next;	//указатель на следующий блок
-	struct virtualSegmentBlock* prev;	//указатель на предыдущий блок
-} segmentBlock;
+/*
 
 typedef struct virtualSegment			//виртуальный сегмент
 {
-	int segmNum;						//номер сегмента
-	virtualSegmentBlock* head;			//указатель на начало сегмента
-	virtualSegmentBlock* tail;			//указатель на конец сегмента
+	int number;							//номер сегмента
 	struct virtualSegment* next;		//указатель на следующий сегмент
-	struct virtualSegment* prev;		//указатель на предыдущий сегмент
 } virtualSegment;
 
-typedef struct virtualSegmentsList		//список виртуальных сегментов
-{
-	virtualSegment* head;				//указатель на начало списка
-	virtualSegment* tail;				//указатель на конец списка
-} virtualSegmentsList;
+*/
 
-typedef struct segmentTableCell			//ячейка таблицы сегментов
+typedef struct segmentsList				//список сегментов
 {
-	int segmNum;						//номер виртуального сегмента
-	VA physAddr;						//указатель на соответствующий сегмент в ОП
+	int number;							//номер виртуального сегмента (МОЖЕТ ЛУЧШЕ СДЕЛАТЬ НОМЕР ФИЗ.БЛОКА???)
+	VA physAddr;						//указатель на расположение в ОП
 	long segmentSize;					//размер сегмента
 	unsigned int modification;			//бит модификации сегмента
 	unsigned int presence;				//бит присутствия сегмента в ОП
-	struct segmentTableCell* next;		//указатель на следующий элемент таблицы
-	struct segmentTableCell* prev;		//указатель на предыдущий элемент таблицы
-} segmentTableCell;
-
-typedef struct segmentTable				//таблица сегментов
-{
-	int tableSize;						//размер таблицы
-	segmentTableCell* head;				//указатель на начало таблицы
-	segmentTableCell* tail;				//указатель на конец таблицы
-} segmentTable;
+	struct segmentsList* next;			//указатель на следующий элемент
+} segmentsList;
 
 physicalMemory physMem;
-segmentTable *table;
+segmentsList* list;
 
 
 
@@ -173,11 +161,15 @@ int _init (int n, int szPage)
 
 	errCode = _init_physical_memory(n, szPage);			//инициализируем ОП
 
-	errCode = _create_table();							//создаём таблицу
+														//инициализируем память под жд
+
+														//инициализируем память под кэш
+
+	/*errCode = _create_table();							//создаём таблицу
 
 	for(iter = 0; iter < n && errCode == 1; iter++){	//вставляем в таблицу n полей длины szPage
 		errCode = _push(NULL, szPage);
-	}
+	}*/
 
 	return errCode;
 }
@@ -263,7 +255,7 @@ int _pushTableCell(VA* dataPtr, size_t szPage)  //добавляем ячейку в конец табли
 
 
 
-int _pushSegmentBlock(long virtualAddr, VA* physAddr, segment* segm)	//добавляем блок в конец сегмента
+int _pushSegmentBlock(long virtualAddr, VA* physAddr, virtualSegment* segm)	//добавляем блок в конец сегмента
 {
 	int errCode;
 
@@ -298,6 +290,83 @@ int _pushSegmentBlock(long virtualAddr, VA* physAddr, segment* segm)	//добавляем
 
 
 
-int _malloc (VA* ptr, size_t szBlock){
+int _malloc (VA* ptr, size_t szBlock)
+{
+	ptr = _find_free_space(szBlock);
+	_add_new_block();
+}
+
+
+
+int _add_new_block()
+{
 	
+	int errCode;
+
+	physicalBlock* blc = phys;	//выделяем память под блок
+
+	if(!blc){										//проверка выделения памяти
+		errCode = -1;
+	}
+	else{
+		errCode = 1;
+
+		VA* physAddr = (VA*) malloc(sizeof(VA));	//выделяем физ. память
+
+		blc -> virtualAddr = virtualAddr;			//присваем адрес блока сегмента
+		blc -> physAddr = physAddr;					//присваем размер сегмента
+		blc -> next = NULL;							//новая ячейка ссылается на нулевую ячейку
+		blc -> prev = segment -> tail;				//а также на предыдую ячейку таблицы
+
+		if(table -> head){							//если таблица не пуста
+			table -> tail -> next = tmp;			//ссылаемся на новую ячейку из последней ячейки 
+			table -> tail = tmp;					//и добавляем новую ячейку в конец таблицы
+		}
+		else{										//если таблица пуста
+			table -> head = table -> tail = tmp;	//новая ячейка становится одновременно первой и последней
+		}
+
+		table -> tableSize++;						//увеличиваем рамер таблицы
+	}
+
+	return errCode;
+}
+
+
+
+VA* _find_free_space(size_t szBlock)
+{
+	if(physMem.space == NULL){
+		return NULL;
+	}
+	else{
+		if(physMem.head == NULL){
+			return &physMem.space;
+		}
+		else{
+			physicalBlock* curBlock = physMem.head;
+			physicalBlock* nextBlock = curBlock -> next;
+			int beginOfSpace, endOfSpace, space;
+			bool searchingAnswer = false;
+
+			while(curBlock -> next != NULL){
+				beginOfSpace = curBlock -> offset + curBlock -> size;
+				endOfSpace = nextBlock -> offset;
+				space = endOfSpace - beginOfSpace;
+
+				if(space >= szBlock){
+					return (&physMem.space + beginOfSpace);
+				}
+			}
+
+			space = physMem.size - beginOfSpace;
+
+			if(space >= szBlock){
+				return (&physMem.space + beginOfSpace);
+			}
+			else{
+				return NULL;
+			}
+		}
+	}
 }
